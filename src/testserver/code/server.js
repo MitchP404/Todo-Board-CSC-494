94d90de33f1da44a1eef5945f0bc1c47d210f1e4
MitchP404 <75387517+MitchP404@users.noreply.github.com>
2026-02-24 22:37:56 -0500

    Added comments to server.js and minor adjustments to index.html and style.css

--- src/testserver/code/server.js
+++ src/testserver/code/server.js
@@ -8,12 +8,12 @@ import { WebSocket, WebSocketServer } from 'ws';
 const app = express(); //Initialize an express server
 const port = /*process.env.PORT ||*/ 3000; // Use either the PORT environment variable or port 3000
 
-// Error handler for errors that occur when HTTP server is handling issue
+// Error handler for errors that occur when HTTP server is handling issue (pre-upgrade)
 function onSocketPreError(e) {
     console.log(e);
 }
 
-// Error handler for errors that occur when wss is handling issue
+// Error handler for errors that occur when wss is handling issue (post-upgrade)
 function onSocketPostError(e) {
     console.log(e);
 }
@@ -24,28 +24,38 @@ const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
+// The web socket server that runs alongside the HTTP server
 const wss = new WebSocketServer({ noServer: true });
 
+// Moves a socket into wss after performing authentication on the HTTP server
 server.on('upgrade', (req, socket, head) => {
     socket.on('error', onSocketPreError);
 
     // perform auth
+    /*
     if(!!req.headers['BadAuth']) { //TODO: replace with actual authentication
         socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
         socket.destroy();
         return;
     }
+    */
 
+    //Give the socket to wss
     wss.handleUpgrade(req, socket, head, (ws) => {
+        //We don't want to use the PreError handler any more
         socket.removeListener('error', onSocketPreError);
+        //Start a connection on the web socket
         wss.emit('connection', ws, req);
     });
 
 });
 
+// Runs when doing a new web socket connection
 wss.on('connection', (ws, req) => {
+    //Set the error handler to PostError
     ws.on('error', onSocketPostError);
 
+    //Set what to do when a message is received
     ws.on('message', (msg, isBinary) => {
         wss.clients.forEach((client) => {
             if(client.readyState === WebSocket.OPEN) {
