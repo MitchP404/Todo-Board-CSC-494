9b0b0a8adfa8460898a4c968ce7a4ba1cad4c630
MitchP404 <75387517+MitchP404@users.noreply.github.com>
2026-03-02 08:50:05 -0500

    Theoretically made the sql database be created automatically

---
+++ src/mysqldatabase/initdb.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ToDoBoardData;
+USE ToDoBoardData;
+CREATE TABLE IF NOT EXISTS ToDoItems(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    status BOOL,
+    active_time DATETIME
+);
